c87b32d5716518950a98300e49801a06adadc090
Thibaut Roland <thibaut.roland2@gmail.com>
2024-02-26 15:39:03 +0100

    deleted ruby app, started node express with typecript

--- backend/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-
-    has_secure_password :password, validations: true
-    validates :email, presence: true, uniqueness: true
-    validates :first_name, :last_name, presence: true
-end
-
