dd141ab4f5968ff72bffe559f8b673e31ea6d393
Thibaut Roland <thibaut.roland2@gmail.com>
2024-03-13 15:18:26 +0100

    changed text in taskCard depending on whether the task is assigned or not

--- frontend/src/components/taskCard.tsx
+++ frontend/src/components/taskCard.tsx
@@ -1,10 +1,14 @@
 import { task } from "@/interfaces/tasks"
+import { useState } from "react"
 
 type Props = {
     task: task
 }
 
 export const TaskCard = ({task}: Props) => {
+    const [assignedState, setAssignedState] = useState(task.assigned_user_first_name !== null)
+    console.log(task.assigned_user_first_name !== null)
+
     return     <div className="flex flex-col bg-slate-100 p-10">
     <p>task_id: {task.id}</p>
     <p>{task.name}</p>
@@ -12,6 +16,11 @@ export const TaskCard = ({task}: Props) => {
     <p>deadline: {task.deadline.toString()}</p>
     <p>priority level: {task.priority}</p>
     <p>status: {task.status}</p>
-    <p>assigned to {task.assigned_user_first_name} {task.assigned_user_last_name}</p>
+    <p className={assignedState ? "" : "hidden"}>
+        assigned to {task.assigned_user_first_name} {task.assigned_user_last_name}
+    </p>
+    <p className={!assignedState ? "" : "hidden"}>
+        Not assigned
+    </p>
 </div>
 }
