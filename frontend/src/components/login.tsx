8c97beb75824b632ca312df2a8cc8e3b01c5e291
IPreferWater <ipreferwater2@gmail.com>
2024-04-04 22:30:21 +0200

    feat(front): do the login page

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,9 +1,11 @@
 
 
 export const LoginSucces = () => {
-    return     <div className="flex border-slate-400 bg-slate-100 p-10">
-        <p>Bravo vous allez bientôt être redirigé</p>
-  </div>
+    return      <div className="w-96 px-8 py-4 bg-white border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] grid place-content-center">
+    <div>
+       <h1 className="text-2xl mb-4">Welcome back friend !</h1>
+    </div>
+   </div>
 }
 
 export const LoginBackError = () => {
